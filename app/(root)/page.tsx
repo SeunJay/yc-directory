7db84cd3568c054c3f12b130eb01a933e065da34
Oluwaseun Jayeoba <seunjay92@gmail.com>
2024-11-09 12:21:32 +0100

    card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,69 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _id: 1,
+      views: 55,
+      description: "This is a description",
+      category: "Robots",
+      title: "We Robots",
+      image:
+        "https://res.cloudinary.com/dhfsofugy/image/upload/v1722230104/hero-slider-3_ijkyov.webp",
+      author: {
+        _id: 1,
+        name: "John Smith",
+      },
+      createdAt: new Date(),
+    },
+  ];
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Votes on Pitches, and Get Noticed in Virtual
+          Competitions
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <StartupCard
+                _id={post._id}
+                views={post.views}
+                author={post.author}
+                description={post.description}
+                category={post.category}
+                title={post.title}
+                image={post.image}
+                createdAt={post.createdAt}
+                key={post._id}
+              />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
---
+++ components/StartupCard.tsx
@@ -0,0 +1,89 @@
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+interface Props {
+  _id: number;
+  views: number;
+  description: string;
+  author: {
+    _id: number;
+    name: string;
+  };
+  category: string;
+  title: string;
+  image: string;
+  createdAt: Date;
+}
+
+const StartupCard = ({
+  _id,
+  views,
+  title,
+  author,
+  image,
+  createdAt,
+  description,
+  category,
+}: Props) => {
+  return (
+    <div className="startup-card group">
+      <div className="flex-between">
+        <p className="startuo_card_date">{formatDate(createdAt.toString())}</p>
+
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${author._id}`}>
+            <p className="text-16-medium line-clamp-1">{author.name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+
+        <Link href={`/user/${author._id}`}>
+          <Image
+            src="https://placehold.co/48x48"
+            alt="avatar"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+
+        <Image
+          src={image}
+          className="startup-card_img"
+          alt="cover"
+          width={100}
+          height={100}
+        />
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default StartupCard;
